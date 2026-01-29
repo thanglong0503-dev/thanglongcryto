7ebe2bfbd854d247fcca85c2ec079011358dee80
thanglong0503-dev <thanglong0503@gmail.com>
2026-01-29 23:16:09 +0700

    Update charts.py

--- frontend/charts.py
+++ frontend/charts.py
@@ -1,23 +1,18 @@
 import streamlit.components.v1 as components
 
-def render_chart(symbol):
+def render_chart(symbol, height=900): # <--- Thêm tham số height
     # LOGIC CHỌN NGUỒN BIỂU ĐỒ
-    if symbol == 'GC=F':
-        tv_symbol = "TVC:GOLD"     # Biểu đồ Vàng thế giới
-    elif symbol == 'CL=F':
-        tv_symbol = "TVC:USOIL"    # Biểu đồ Dầu
-    elif symbol == '^GSPC':
-        tv_symbol = "TVC:SPX"      # S&P 500
-    elif symbol == 'EURUSD=X':
-        tv_symbol = "FX:EURUSD"    # Forex
+    if symbol == 'GC=F': tv_symbol = "TVC:GOLD"
+    elif symbol == 'CL=F': tv_symbol = "TVC:USOIL"
+    elif symbol == '^GSPC': tv_symbol = "TVC:SPX"
+    elif symbol == 'EURUSD=X': tv_symbol = "FX:EURUSD"
     else:
-        # Mặc định là Crypto (Binance)
-        clean_sym = symbol.replace('/', '').replace('-', '')
+        clean_sym = symbol.replace('/', '').replace('-', '').replace('USD', '')
         tv_symbol = f"BINANCE:{clean_sym}USDT"
     
     html_code = f"""
-    <div class="tradingview-widget-container" style="height:900px;width:100%">
-      <div id="tv_chart" style="height:100%;width:100%"></div>
+    <div class="tradingview-widget-container" style="height:100%;width:100%">
+      <div id="tv_chart_{symbol}" style="height:{height}px;width:100%"></div>
       <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
       <script type="text/javascript">
       new TradingView.widget({{
@@ -32,15 +27,14 @@ def render_chart(symbol):
       "enable_publishing": false,
       "backgroundColor": "#050505",
       "gridColor": "rgba(0, 243, 255, 0.05)",
-      "hide_top_toolbar": false,
-      "container_id": "tv_chart",
+      "hide_top_toolbar": { 'true' if height < 500 else 'false' }, /* Ẩn toolbar nếu chart nhỏ */
+      "container_id": "tv_chart_{symbol}",
       "studies": [
         "BB@tv-basicstudies",
-        "RSI@tv-basicstudies",
-        "VbPFixed@tv-basicstudies"
+        "RSI@tv-basicstudies"
       ]
       }});
       </script>
     </div>
     """
-    components.html(html_code, height=910)
+    components.html(html_code, height=height + 10)
